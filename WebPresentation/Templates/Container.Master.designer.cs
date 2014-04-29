3ef6d3b3661658cf56d3daa9073ecf82e4c1a894
Lochlan Bunn <loklaan@gmail.com>
2014-04-29 19:23:23 +1000

    Retrieved missing designers file for main master page.

--- WebPresentation/Templates/Container.Master.designer.cs
+++ WebPresentation/Templates/Container.Master.designer.cs
@@ -7,36 +7,20 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace webPresentation.Templates {
-    
-    
-    public partial class container {
-        
+namespace WebPresentation.Templates
+{
+
+
+    public partial class Container
+    {
+
         /// <summary>
-        /// head control.
+        /// Main control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Main;
     }
 }
