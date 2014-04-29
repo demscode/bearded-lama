f432f14fa22a2f8c627f1c91258d8fe77564319b
Lochlan Bunn <loklaan@gmail.com>
2014-04-29 19:22:19 +1000

    Added 'index' aspx page.
    Moved some files around.

--- WebPresentation/Views/Index.aspx.cs
+++ WebPresentation/Views/Index.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace webPresentation.Templates
+namespace WebPresentation.Views
 {
-    public partial class container : System.Web.UI.MasterPage
+    public partial class Index : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
