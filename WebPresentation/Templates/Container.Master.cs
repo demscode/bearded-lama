a29fcd3d5f60193d1ba1889544b133b893285939
Lochlan Bunn <loklaan@gmail.com>
2014-04-29 19:22:19 +1000

    Added 'index' aspx page.
    Moved some files around.

---
+++ WebPresentation/Templates/Container.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebPresentation.Templates
+{
+    public partial class Container : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebPresentation/Views/Index.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Templates/Container.Master" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebPresentation.Views.Index" %>
+<asp:Content ID="MainIndex" ContentPlaceHolderID="Main" runat="server">
+</asp:Content>
