bbd1bd86b637088d49848dd4aa2216a5a35a7a5f
Lochlan Bunn <loklaan@gmail.com>
2014-04-30 12:57:58 +1000

    Amended Dataset references.

--- DataAccess/Properties/Settings.Designer.cs
+++ DataAccess/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace DataAccess.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(local);Initial Catalog=bearded_lama")
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(local);Initial Catalog=bearded_lama")]
             
         public string DefaultConnectionString {
             get {
