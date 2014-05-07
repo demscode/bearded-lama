a549b2b532a4c94d28d674a62f3ee3d957621d8a
Lochlan Bunn <loklaan@gmail.com>
2014-05-07 14:40:06 +1000

    Added properties to MembershipProvider

--- BusinessLogic/AccountsMembershipProvider.cs
+++ BusinessLogic/AccountsMembershipProvider.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+using System.Configuration.Provider;
+using System.Web;
+using System.Web.Security;
+using DataAccess;
+using DataSets;
 
 namespace BusinessLogic
 {
@@ -9,21 +15,118 @@ namespace BusinessLogic
     /// Manages storage of user account information, tailored to utilise data
     /// access in the Data Tier of this application.
     /// </summary>
-    class AccountsMembershipProvider : System.Web.Security.MembershipProvider
+    class AccountsMembershipProvider : MembershipProvider
     {
-
-        public override string ApplicationName
+        //
+        // Initialise the Provider with important configuration
+        //
+        public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
         {
-            get
+            base.Initialize(name, config); // ProviderBase method tracks call count
+
+            // Assignments from configuration file
+            pApplicationName = GetConfigValue(config["applicationName"], System.Web.Hosting.HostingEnvironment.ApplicationVirtualPath);
+            pMaxInvalidPasswordAttempts = Convert.ToInt32(GetConfigValue(config["maxInvalidPasswordAttempts"], "5"));
+            pPasswordAttemptWindow = Convert.ToInt32(GetConfigValue(config["passwordAttemptWindow"], "10"));
+            pMinRequiredNonAlphanumericCharacters = Convert.ToInt32(GetConfigValue(config["minRequiredNonAlphanumericCharacters"], "1"));
+            pMinRequiredPasswordLength = Convert.ToInt32(GetConfigValue(config["minRequiredPasswordLength"], "7"));
+            pPasswordStrengthRegularExpression = Convert.ToString(GetConfigValue(config["passwordStrengthRegularExpression"], ""));
+            pEnablePasswordReset = Convert.ToBoolean(GetConfigValue(config["enablePasswordReset"], "true"));
+            pEnablePasswordRetrieval = Convert.ToBoolean(GetConfigValue(config["enablePasswordRetrieval"], "true"));
+            pRequiresQuestionAndAnswer = Convert.ToBoolean(GetConfigValue(config["requiresQuestionAndAnswer"], "false"));
+            pRequiresUniqueEmail = Convert.ToBoolean(GetConfigValue(config["requiresUniqueEmail"], "true"));
+
+            switch (config["passwordFormat"])
             {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
+                case null:
+                case "Hashed":
+                    pPasswordFormat = MembershipPasswordFormat.Hashed;
+                    break;
+                case "Encrypted":
+                    pPasswordFormat = MembershipPasswordFormat.Encrypted;
+                    break;
+                case "Clear":
+                    pPasswordFormat = MembershipPasswordFormat.Clear;
+                    break;
+                default:
+                    throw new ProviderException("Password format not accepted");
             }
         }
 
+        //
+        // Properties
+        //
+        private string pApplicationName;
+        private bool pEnablePasswordReset;
+        private bool pEnablePasswordRetrieval;
+        private bool pRequiresQuestionAndAnswer;
+        private bool pRequiresUniqueEmail;
+        private int pMaxInvalidPasswordAttempts;
+        private int pPasswordAttemptWindow;
+        private MembershipPasswordFormat pPasswordFormat;
+        private int pMinRequiredPasswordLength;
+        private string pPasswordStrengthRegularExpression;
+        private int pMinRequiredNonAlphanumericCharacters;
+
+        public override string ApplicationName
+        {
+            get { return pApplicationName; }
+            set { pApplicationName = value; }
+        }
+
+        public override bool EnablePasswordReset
+        {
+            get { return pEnablePasswordReset; }
+        }
+
+        public override bool EnablePasswordRetrieval
+        {
+            get { return pEnablePasswordRetrieval; }
+        }
+
+        public override bool RequiresQuestionAndAnswer
+        {
+            get { return pRequiresQuestionAndAnswer; }
+        }
+
+        public override bool RequiresUniqueEmail
+        {
+            get { return pRequiresUniqueEmail; }
+        }
+
+        public override int MaxInvalidPasswordAttempts
+        {
+            get { return pMaxInvalidPasswordAttempts; }
+        }
+
+        public override int PasswordAttemptWindow
+        {
+            get { return pPasswordAttemptWindow; }
+        }
+
+        public override MembershipPasswordFormat PasswordFormat
+        {
+            get { return pPasswordFormat; }
+        }
+
+        public override int MinRequiredNonAlphanumericCharacters
+        {
+            get { return pMinRequiredNonAlphanumericCharacters; }
+        }
+
+        public override int MinRequiredPasswordLength
+        {
+            get { return pMinRequiredPasswordLength; }
+        }
+
+        public override string PasswordStrengthRegularExpression
+        {
+            get { return pPasswordStrengthRegularExpression; }
+        }
+
+        //
+        // Implemented methods from MembershipProvider
+        //
         public override bool ChangePassword(string username, string oldPassword, string newPassword)
         {
             throw new NotImplementedException();
@@ -42,16 +145,6 @@ namespace BusinessLogic
         public override bool DeleteUser(string username, bool deleteAllRelatedData)
         {
             throw new NotImplementedException();
-        }
-
-        public override bool EnablePasswordReset
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override bool EnablePasswordRetrieval
-        {
-            get { throw new NotImplementedException(); }
         }
 
         public override System.Web.Security.MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
@@ -94,46 +187,6 @@ namespace BusinessLogic
             throw new NotImplementedException();
         }
 
-        public override int MaxInvalidPasswordAttempts
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override int MinRequiredNonAlphanumericCharacters
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override int MinRequiredPasswordLength
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override int PasswordAttemptWindow
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override System.Web.Security.MembershipPasswordFormat PasswordFormat
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override string PasswordStrengthRegularExpression
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override bool RequiresQuestionAndAnswer
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override bool RequiresUniqueEmail
-        {
-            get { throw new NotImplementedException(); }
-        }
-
         public override string ResetPassword(string username, string answer)
         {
             throw new NotImplementedException();
@@ -152,6 +205,22 @@ namespace BusinessLogic
         public override bool ValidateUser(string username, string password)
         {
             throw new NotImplementedException();
+        }
+
+        //
+        // Helper functions
+        //
+        private string GetConfigValue(string configValue, string defaultValue)
+        {
+            if (String.IsNullOrEmpty(configValue))
+            {
+                return defaultValue;
+            }
+
+            return configValue;
+            {
+
+            }
         }
     }
 }
