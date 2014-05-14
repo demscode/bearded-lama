8a0300a993a87498daf05f956e4cc6789dbefbf3
Andres Sepulveda <andresdsep@yahoo.es>
2014-05-14 15:44:07 +1000

    Got rid of an unused reference.

--- BusinessLogic/GameRecommendations.cs
+++ BusinessLogic/GameRecommendations.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data;
-using System.Web;
 using DataAccess.GamesTableAdapters;
 using DataAccess.GameHistoryTableAdapters;
 using DataAccess.UsersTableAdapters;
