a9623e6cb42865bf543ddbdb0a9e88e18aa7b841
Lochlan Bunn <loklaan@gmail.com>
2014-05-21 00:30:48 +1000

    Added NuGet configuration and empty static Game Submit class.

---
+++ BusinessLogic/Games/Submit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ionic.Zip;
+
+namespace BusinessLogic.Games
+{
+   static class Submit
+    {
+    }
+}
