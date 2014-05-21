08e6ec757be4c00a98ee3c6805158833105c1286
Andres Sepulveda <andresdsep@yahoo.es>
2014-05-21 20:04:35 +1000

    Preliminary implementation of the Game page.
    Pending refactoring for the recommended games control

---
+++ WebPresentation/Views/Game.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="Game" Language="C#" MasterPageFile="~/Templates/Container.Master" AutoEventWireup="true" CodeBehind="Game.aspx.cs" Inherits="WebPresentation.Views.Game" %>
+<asp:Content ID="MainGame" ContentPlaceHolderID="Main" runat="server">
+    <section class="container">
+        <h1 id="GamePageHeader" class="page-header" runat="server"></h1>
+
+        <section class="container">
+            <div class="row">
+                <h2>Details</h2>
+                <dl class="dl-horizontal">
+                    <dt>Description</dt>
+                    <dd id="gameDescription" runat="server"></dd>
+                    <dt>Category</dt>
+                    <dd id="gameCategory" runat="server"></dd>
+                    <dt>Tags</dt>
+                    <dd id="gameTags" runat="server"></dd>
+                    <dt>Rating</dt>
+                    <dd id="gameRating" runat="server"></dd>
+                    <dt>Uploader</dt>
+                    <dd id="gameUploader" runat="server"></dd>
+                    <dt>Date uploaded</dt>
+                    <dd id="gameUpDate" runat="server"></dd>
+                    <dt>Restricted</dt>
+                    <dd id="gameRestrict" runat="server"></dd>
+                </dl>
+            </div>
+            <div class="row">
+                <h2>Similar Games</h2>
+                <section class="container-fluid">
+                    <div class="row">
+                        <div class="col-xs-6 col-md-3" id="recommendedGame1" runat="server">
+                        </div>
+                        <div class="col-xs-6 col-md-3" id="recommendedGame2" runat="server">
+                        </div>
+                        <div class="col-xs-6 col-md-3" id="recommendedGame3" runat="server">
+                        </div>
+                        <div class="col-xs-6 col-md-3" id="recommendedGame4" runat="server">
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </section>
+    </section>
+
+</asp:Content>
---
+++ WebPresentation/Views/Game.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLogic;
+using DataSets;
+
+namespace WebPresentation.Views {
+    public partial class Game : System.Web.UI.Page {
+
+        protected int gameId;
+
+        protected void Page_Load(object sender, EventArgs e) {
+            gameId = Convert.ToInt32(Page.RouteData.Values["id"]);
+            Games.GamesRow gameData = GameAccess.GetGameById(gameId);
+            // Pending getting username from userId
+            //long gamerId = long.Parse((userTable.Select("userName = '" + user + "'")[0])["userId"].ToString());
+
+            GamePageHeader.InnerText = gameData.gameName;
+            gameDescription.InnerText = gameData.gameDesc;
+            gameCategory.InnerText = gameData.category;
+            gameTags.InnerText = gameData.tags;
+            gameRating.InnerText = gameData.rating.ToString();
+            gameUploader.InnerText = gameData.userId.ToString();// replace with username
+            gameUpDate.InnerText = gameData.subDate.ToShortDateString();
+            if (gameData.restrict) {
+                gameRestrict.InnerText = "Yes";
+            } else {
+                gameRestrict.InnerText = "No";
+            }
+
+            BusinessLogic.Accounts.User user = (BusinessLogic.Accounts.User)System.Web.Security.Membership.GetUser();
+
+            Games.GamesDataTable table = GameRecommendations.GetGamePageRecommendations(gameId, user.UserName);
+
+            recommendedGame1.InnerHtml = GameBoxOutput(table[0]);
+            recommendedGame2.InnerHtml = GameBoxOutput(table[1]);
+            recommendedGame3.InnerHtml = GameBoxOutput(table[2]);
+            recommendedGame4.InnerHtml = GameBoxOutput(table[3]);
+        }
+
+        private string GameBoxOutput(Games.GamesRow game) {
+            return "<article class=\"thumbnail game-short\">" +
+                "<img src=\"http://placehold.it/600x150\" class=\"img-responsive img-rounded\"" +
+                "alt=\"placeholder for game image if avail\" />" +
+                "<button class=\"btn btn-primary btn-sm btn-block\" type=\"button\" onclick=\"window.location='/g/" +
+                game.gameId.ToString() + "';\">Play »</button>" +
+                "<hr />" +
+                "<h3>" + game.gameName + " <small>by " + game.userId.ToString() + "</small></h3>" + // pending actually username
+                "<p>" + game.gameDesc + "</p>" +
+                "</article>";
+        }
+    }
+}
