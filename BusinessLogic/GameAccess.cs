57f3f7926f50927f75aee5aeb08d67b33674e973
Andres Sepulveda <andresdsep@yahoo.es>
2014-05-22 21:00:15 +1000

    Added documentation for GameAccess class.

--- BusinessLogic/GameAccess.cs
+++ BusinessLogic/GameAccess.cs
@@ -10,9 +10,17 @@ using DataSets;
 
 namespace BusinessLogic {
 
+    /// <summary>
+    /// Business tier class for basic access to 'game' table data.
+    /// </summary>
     [System.ComponentModel.DataObject]
     public class GameAccess {
 
+        /// <summary>
+        /// Gets a game from a provided game ID.
+        /// </summary>
+        /// <param name="gameId">gameId to be found</param>
+        /// <returns>Matched game</returns>
         [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select)]
         public static DataSets.Games.GamesRow GetGameById(int gameId) {
             GamesTableAdapter gamesTableAdapter = new GamesTableAdapter();
@@ -20,12 +28,26 @@ namespace BusinessLogic {
             return allGames.FindBygameId(gameId);
         }
 
+        /// <summary>
+        /// Updates a game entry in the database.
+        /// </summary>
+        /// <param name="game">game to be updated</param>
         [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Update)]
         public static void UpdateGame(DataRow game) {
             GamesTableAdapter adapter = new GamesTableAdapter();
             adapter.Update(game);
         }
 
+        /// <summary>
+        /// Inserts a newly submitted game into the database.
+        /// </summary>
+        /// <param name="gameName"></param>
+        /// <param name="gameDesc"></param>
+        /// <param name="tags"></param>
+        /// <param name="category"></param>
+        /// <param name="publicPackage"></param>
+        /// <param name="userId"></param>
+        /// <param name="restrict"></param>
         [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Insert)]
         public static void InsertProduct(string gameName, string gameDesc, string tags, string category, 
             object publicPackage, long userId, bool restrict) {
@@ -36,12 +58,22 @@ namespace BusinessLogic {
             adapter.Insert(gameName, gameDesc, tags, category, publicPackage, 0, userId, thisDay, null, restrict);
         }
 
+        /// <summary>
+        /// Deletes a game entry from the database.
+        /// (revision due, conflicts might occur with foreing keys)
+        /// </summary>
+        /// <param name="original_ProductId">ID of the game to be deleted</param>
         [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Delete)]
         public static void DeleteProduct(long original_ProductId) {
             GamesTableAdapter adapter = new GamesTableAdapter();
             adapter.Delete(original_ProductId);
         }
 
+        /// <summary>
+        /// Utility method for matching a game ID with the username of the uploader.
+        /// </summary>
+        /// <param name="gameId">ID to be found</param>
+        /// <returns>userName of the uploader</returns>
         public static string GetUploader(int gameId) {
             GamesTableAdapter gameAdapter = new GamesTableAdapter();
             UsersTableAdapter userAdapter = new UsersTableAdapter();
