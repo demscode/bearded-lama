1d78aab7d58faa114682c4178ab395069f2fdbe9
Lochlan Bunn <loklaan@gmail.com>
2014-05-24 16:50:26 +1000

    Implemented validation method in Package for packages json configuration.
    * Added Unit Tests project
    * Added tests for new method

--- BusinessLogic/Games/Package.cs
+++ BusinessLogic/Games/Package.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using Ionic.Zip;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Linq;
 
 namespace BusinessLogic.Games
 {
@@ -13,14 +17,122 @@ namespace BusinessLogic.Games
     /// are supplied by the user to give information about the available game files, game entry points and
     /// game metadata like gaming instructions.
     /// </summary>
-   static class Package
+    static public class Package
     {
-       // Submit Game Package
+        // json-schema.org
+        // TODO: move as file to top level of solution, and as configuration setting
+        static private string packageSchema = @"
+{
+    'description': 'Game Package schema for Bearded Lama',
+    'type': 'object',
+    'properties': {
+        'name': {
+            'type': 'string'
+        },
+        'identifier': {
+            'type': 'string'
+        },
+        'information': {
+            'type': 'string'
+        },
+        'instruction': {
+            'type': 'string'
+        },
+        'data': {
+            'description': 'Files required for the Game',
+            'type': 'object',
+            'required': true,
+            'js': {
+                'type': 'array',
+                'items': {
+                    'type': 'string'
+                },
+                'minItems': 1,
+                'required': true
+            }
+        },
+        'entry': {
+            'description': 'Properties for the HTML entry point of the Game',
+            'element': {
+                'type': 'string',
+                'required': true
+            },
+            'elementid': {
+                'type': 'string',
+                'required': true
+            },
+            'height': {
+                'type': 'string',
+                'required': true
+            },
+            'width': {
+                'type': 'string',
+                'required': true
+            }
+        }
+    }
+}";
+        // Submit Game Package
 
-       // Validate Game Package
-       // Validate Game PackageJson
-       // Create Game Package
-       // Update Game Package
-       // Update Game Information
+        // Validate Game Package
+
+        /// <summary>
+        /// Initialises a new instance of the Package.PackageInfo class from the specified TextReader.
+        /// </summary>
+        /// <param name="jsonReader">TextReader for a JSON package, matching define schema.</param>
+        /// <returns></returns>
+        /// <exception cref="JsonSchemaException">Contains schema validation messages in e.Data["validationMessages"]</exception>
+        static public PackageInfo ValidateConfiguration(TextReader jsonReader)
+        {
+            using (JsonValidatingReader validatingReader = new JsonValidatingReader(new JsonTextReader(jsonReader)))
+            {
+                JsonSchema schema = JsonSchema.Parse(packageSchema);
+                validatingReader.Schema = schema;
+                IList<string> messages = new List<string>();
+                validatingReader.ValidationEventHandler += (o, a) => messages.Add(a.Message);
+
+                JsonSerializer serialiser = new JsonSerializer();
+                PackageInfo deserialisedPackageInfo = serialiser.Deserialize<PackageInfo>(validatingReader);
+
+                if (messages.Count != 0)
+                {
+                    JsonSchemaException e = new JsonSchemaException();
+                    e.Data.Add("validationMessages", messages);
+                    throw e;
+                }
+                else
+                {
+                    return deserialisedPackageInfo;
+                }
+            }
+        }
+
+        // Create Game Package
+        // Update Game Package
+        // Update Game Information
+    }
+
+    /// <summary>
+    /// An instance of a deserialised json configuration, bearded-lama.json
+    /// </summary>
+    public class PackageInfo
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("identifier")]
+        public string Id { get; set; }
+
+        [JsonProperty("information")]
+        public string Description { get; set; }
+
+        [JsonProperty("instruction")]
+        public string Instruction { get; set; }
+
+        [JsonProperty("data")]
+        public Dictionary<String, List<string>> Data { get; set; }
+
+        [JsonProperty("entry")]
+        public Dictionary<String, String> EntryPoint { get; set; }
     }
 }
---
+++ UnitTests/GamePackageTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic.Games;
+using Newtonsoft.Json.Schema;
+using System.IO;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class GamePackageTests
+    {
+        // TEST RESOURCES
+
+        private string validIncompleteJsonString = @"
+{
+    'data': {
+        'js': [
+            '/bin/game.js',
+            '/bin/levels.js'
+        ]
+    },
+    'entry': {
+        'element': 'canvas',
+        'id': 'gameEntry',
+        'height': '400',
+        'width': '600'
+    }
+}";
+        private string validCompleteJsonString = @"
+{
+    'name': 'coolGame',
+    'identifier': '1',
+    'information': 'This game includes graphic violence.',
+    'instruction': 'Press q to quit',
+    'data': {
+        'js': [
+            '/bin/game.js',
+            '/bin/levels.js'
+        ]
+    },
+    'entry': {
+        'element': 'canvas',
+        'id': 'gameEntry',
+        'height': '400',
+        'width': '600'
+    }
+}";
+        private string invalidJsonString = @"
+{
+    'name': 'coolGame',
+    'identifier': '1',
+    'information': 'This game includes graphic violence.',
+    'instruction': 'Press q to quit',
+    'entry': {
+        'element': 'canvas',
+        'id': 'gameEntry',
+        'height': '400',
+        'width': '600'
+    }
+}";
+
+        // TESTS
+
+        [TestMethod]
+        public void TestValidIncompletePackageInfoValidation()
+        {
+            PackageInfo validPackageInfo = Package.ValidateConfiguration(new StringReader(validIncompleteJsonString));
+            Assert.IsNotNull(validPackageInfo);
+        }
+
+        [TestMethod]
+        public void TestValidCompletePackageInfoValidation()
+        {
+            PackageInfo validPackageInfo = Package.ValidateConfiguration(new StringReader(validCompleteJsonString));
+            Assert.IsNotNull(validPackageInfo);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Newtonsoft.Json.Schema.JsonSchemaException))]
+        public void TestInvalidPackageInfoValidation()
+        {
+            PackageInfo invalidPackageInfo = Package.ValidateConfiguration(new StringReader(invalidJsonString));
+            Assert.IsNull(invalidPackageInfo);
+        }
+    }
+}
