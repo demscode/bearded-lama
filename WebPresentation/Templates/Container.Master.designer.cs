6e4447bf70be178dc20f9730028beb79dd60ca72
Andres Sepulveda <andresdsep@yahoo.es>
2014-05-25 13:28:09 +1000

    Changed the button as per your request @Loch

--- WebPresentation/Templates/Container.Master.designer.cs
+++ WebPresentation/Templates/Container.Master.designer.cs
@@ -28,7 +28,7 @@ namespace WebPresentation.Templates {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button searchButton;
+        protected global::System.Web.UI.HtmlControls.HtmlButton searchButton;
         
         /// <summary>
         /// searchTextBox control.
